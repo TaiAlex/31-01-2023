fa2b6297299f9b0de53f71ac486070721765877b
TaiAlex <118870638+TaiAlex@users.noreply.github.com>
2023-02-19 15:34:20 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,55 +1,50 @@
-from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.responses import HTMLResponse
+from fastapi import File, UploadFile, Request, FastAPI, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
-from typing import List
-import pandas as pd
-import shutil as st
 import str_time
+from testAPI import find_path, mp3_to_wav, wav_to_txt, split_list_answer
 import os
-import filetype
-import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-url = "http://127.0.0.1:8000/uploadfile"
-myobj = {'somekey': 'somevalue'}
-x = requests.post(url, json = myobj)
-
-# data = {
-#   "calories": [420, 380, 390],
-#   "duration": [50, 40, 45]
-# }
-
-# #load data into a DataFrame object:
-# df = pd.DataFrame(data)
-# df = df.to_json()
+users = []
 
 time = str_time.now_utc()
 dir_name = str_time.str_yyyy_mm_dd(time)
 file_name = str_time.get_time()
 
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        # contents = file.file.read()
+        # with open("uploaded_" + file.filename, "wb") as f:
+        #     f.write(contents)
+        dir_path = "upload/" + str_time.str_yyyy_mm_dd(time)
+        # str_time.create_path("upload")
+        str_time.create_path(dir_path)
+        type = file.filename.split(".")[1]
+        file_path = dir_path + "/" + f'{file_name}.{type}'
+        # file_path = dir_path + "/" + file.filename
+        # text_path = os.path.join(os.path.dirname(__file__), f'{file_name}.{type}')
+        # print('target_path_1: ', text_path)
+        # print(file_path)
+        file_bytes = file.file.read()
+        # print(file_bytes)
+        str_time.upload_file_bytes(file_bytes, file_path)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()   
+    return {"message": f"Successfuly uploaded {file.filename}"}
 
+@app.get("/to_text")
+def test():
+    path = find_path()
+    t = mp3_to_wav(path)
+    text_id = wav_to_txt(t)
+    text = split_list_answer(text_id)
+    return text
 
-# @app.post("/")
-# async def root(file: UploadFile = File(...)):
-#   with open(f'{file_name}', 'wb') as alex:
-#     st.copyfileobj(file.file, alex)
-#   return {'file_name': alex}
-
-@app.post("/uploadfile/")
-async def upload_file(files: List[UploadFile] = File(...)):
-  for file_in_list in files:
-    dir_path = "upload/" + str_time.str_yyyy_mm_dd(time)
-    str_time.create_path("upload")
-    str_time.create_path(dir_path)
-    file_path = dir_path + "/" + f'{file_name}.wav'
-    file_bytes = file_in_list.file.read()
-    str_time.upload_file_bytes(file_bytes, file_path)
-    # with open(f'{file_path}', 'wb') as alex:
-    #     st.copyfileobj(file_in_list.file, alex)
-    return {'file name': 'Good'}
-    
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def main(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("template.html", {"request": request})
